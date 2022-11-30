06e704f0833abdc721e1171365f1f27f9614cd19
IAIvanoff <autokitsto23@gmail.com>
2022-12-01 02:53:36 +0300

    готова дз

---
+++ dz2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+using static System.Console;
+Write("Введите число ");
+String n = ReadLine();
+int sim =n.Length;
+if (sim==2){
+    WriteLine("третьей цифры нет");
+}
+else WriteLine(n[2].ToString());
